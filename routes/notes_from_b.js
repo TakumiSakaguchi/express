8e32285d4cfcdc0088b02f5ad1990e1d90bce614
阪口巧 <sakaguchitakumi@sakaguchitakuminoMacBook-Air.local>
2025-07-20 09:46:03 +0900

    add env2

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
+//
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://Takumi:n3wv9Rd4JTwG0bnr@test.ykv8aih.mongodb.net/?retryWrites=true&w=majority&appName=test";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
